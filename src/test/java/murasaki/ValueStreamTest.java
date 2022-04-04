d3cd7190f149738d451b1008b341f08e5d782b6b
brianberzins <brianberzins@gmail.com>
2022-04-04 16:12:23 -0500

    r   - extract variable

--- src/test/java/murasaki/ValueStreamTest.java
+++ src/test/java/murasaki/ValueStreamTest.java
@@ -8,14 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ValueStreamTest {
 
+    ValueStream valueStream = new ValueStreamGenerator()
+            .timeToComplete(     () -> Duration.ofHours(7))
+            .timeToBlock(        () -> Duration.ofHours(2))
+            .timeToResolution(   () -> Duration.ofHours(1))
+            .generate();
+
     @Test
     void testValueSteam() {
-        var valueStream = new ValueStreamGenerator()
-                .timeToComplete(     () -> Duration.ofHours(7))
-                .timeToBlock(        () -> Duration.ofHours(2))
-                .timeToResolution(   () -> Duration.ofHours(1))
-                .generate();
         assertEquals(Duration.ofHours(10), valueStream.toComplete());
     }
-
 }
